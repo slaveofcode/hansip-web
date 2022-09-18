75b10f2064081e995e41b2b07e6517d849aee186
Aditya Kresna <slaveofcode@gmail.com>
2022-09-18 15:20:27 +0700

    Add try-catch

--- src/lib/auth/refresh_token.ts
+++ src/lib/auth/refresh_token.ts
@@ -3,25 +3,29 @@ import mem from "mem";
 import { useCookies } from 'vue3-cookies'
 
 export const refreshToken = async (token: string) => {
-  const { cookies } = useCookies()
-  const res = await axios.post("/auth/refresh-token", {
-    token,
-  });
+  try {
+    const { cookies } = useCookies()
+    const res = await axios.post("/auth/refresh-token", {
+      token,
+    });
 
-  if (res.status === 200 && res.data.data) {
-    cookies.remove('access_token')
-    cookies.remove('refresh_token')
+    if (res.status === 200 && res.data.data) {
+      cookies.remove('access_token')
+      cookies.remove('refresh_token')
 
-    const { accessToken, refreshToken, exp, expRefresh } = res.data.data;
-    cookies.set('access_token', accessToken, new Date(exp))
-    cookies.set('refresh_token', refreshToken, new Date(expRefresh))
+      const { accessToken, refreshToken, exp, expRefresh } = res.data.data;
+      cookies.set('access_token', accessToken, new Date(exp))
+      cookies.set('refresh_token', refreshToken, new Date(expRefresh))
 
-    return accessToken
+      return accessToken
+    }
+  } catch (err) {
+    console.log('Refresh token failed:', err)
   }
 
   return false
 };
 
 export const memoizeRefreshToken = mem(refreshToken, {
-  maxAge: 5000,
+  maxAge: 3000,
 })
